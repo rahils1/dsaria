9687d9b1e61ab2b33abc5ace3be0d6e06710d883
rahils1 <rahil15973@gmail.com>
2025-08-06 13:19:59 -0700

    v1.1.0 upload Updated documentation for the project

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Example Package
+# dsaria
 
-This is a simple example package that I have created.
+**A simple Python library for data structures and algorithms.**
+
+`dsaria` is a lightweight Python package that I've created to understand data structures and algorithms, and to learn
+how to package python projects.
+
+---
+
+## Installation
+
+Install directly from PyPI:
+
+```bash
+pip install dsaria
+```
+
+## Current Features
+### Sorting Algorithms
+-Bubble Sort
+-More sorting algorithms coming soon!
+### Data Structures
+-Coming soon!
--- dsaria/sort.py
+++ dsaria/sort.py
@@ -1,4 +1,20 @@
-def bubble_sort(arr):
+from typing import List, Any
+
+def bubble_sort(arr: List[Any]) -> List[Any]:
+    """
+    Sorts a list in ascending order using the bubble sort algorithm.
+
+    Parameters:
+    arr (list): A list of comparable elements (e.g., integers or floats). 
+    If you're passing in a list of objects, make sure that the comparison methods, such as __gt__(self, other), are defined.
+
+    Returns:
+    list: The sorted list in ascending order.
+
+    Example:
+    >>> bubble_sort([3, 1, 2])
+    [1, 2, 3]
+    """
     n = len(arr)
     for i in range(n):
         for j in range(n - i - 1):
